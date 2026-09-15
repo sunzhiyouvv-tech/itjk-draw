00e0fc29f6681468dd9c526367beef2c63cf8c72
sunzhiyouvv-tech <sunzhiyouvv@gmail.com>
2026-09-15 16:56:01 +0800

    feat: simplify ITJK Draw main menu

---
+++ overrides/AppMainMenu.tsx
@@ -0,0 +1,34 @@
+import { MainMenu } from "@excalidraw/excalidraw/index";
+import React from "react";
+
+import type { Theme } from "@excalidraw/element/types";
+
+import { LanguageList } from "../app-language/LanguageList";
+
+export const AppMainMenu: React.FC<{
+  onCollabDialogOpen: () => any;
+  isCollaborating: boolean;
+  isCollabEnabled: boolean;
+  theme: Theme | "system";
+  refresh: () => void;
+}> = React.memo((props) => {
+  return (
+    <MainMenu>
+      <MainMenu.DefaultItems.LoadScene />
+      <MainMenu.DefaultItems.SaveToActiveFile />
+      <MainMenu.DefaultItems.Export />
+      <MainMenu.DefaultItems.SaveAsImage />
+      <MainMenu.DefaultItems.CommandPalette className="highlighted" />
+      <MainMenu.DefaultItems.SearchMenu />
+      <MainMenu.DefaultItems.Help />
+      <MainMenu.DefaultItems.ClearCanvas />
+      <MainMenu.Separator />
+      <MainMenu.DefaultItems.Preferences />
+      <MainMenu.DefaultItems.ToggleTheme allowSystemTheme theme={props.theme} />
+      <MainMenu.ItemCustom>
+        <LanguageList style={{ width: "100%" }} />
+      </MainMenu.ItemCustom>
+      <MainMenu.DefaultItems.ChangeCanvasBackground />
+    </MainMenu>
+  );
+});
